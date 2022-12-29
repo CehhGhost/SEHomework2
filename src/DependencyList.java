3ae2592b63da3641fa73ec391984d1d374e3459d
CehhGhost <ghost5016628@mail.ru>
2022-12-29 21:44:20 +0300

    Create an adjacency list and a method to fill it

--- src/DependencyList.java
+++ src/DependencyList.java
@@ -1,8 +1,25 @@
-import java.io.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class DependencyList {
     public DependencyList(File mainDir) {
         _mainDir = mainDir;
+        SearchThroughDirs(_mainDir);
+    }
+    private void SearchThroughDirs(File dir) {
+        var files = dir.listFiles();
+        if (files != null) {
+            for (var f : files) {
+                if (f.isDirectory()) {
+                    SearchThroughDirs(f);
+                } else if (f.isFile()) {
+
+                }
+            }
+        }
     }
     private final File _mainDir;
+    private Map<File, List<File>> _resultMap;
 }
