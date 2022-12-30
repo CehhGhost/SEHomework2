c2763cf715068e22c3d47eeae265c1c95d9fedc8
CehhGhost <ghost5016628@mail.ru>
2022-12-30 12:41:07 +0300

    Updated GetFilesParents

--- src/DependencyList.java
+++ src/DependencyList.java
@@ -4,14 +4,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class DependencyList {
+public final class DependencyList {
     public DependencyList(File mainDir) {
         _mainDir = mainDir;
         _resultMap = new HashMap<String, List<String>>();
         _resultMap.put(null, new ArrayList<String>());
         SearchThroughDirs(_mainDir);
+        for (var key : _resultMap.keySet()) {
+            if (key != null) {
+                System.out.print(new File(key).getName());
+            }
+            System.out.print(": ");
+            for (var value : _resultMap.get(key)) {
+                System.out.print(new File(value).getName() + " ");
+            }
+            System.out.print("\r\n");
+        }
     }
-    private void SearchThroughDirs(File dir){
+
+    private void SearchThroughDirs(File dir) {
         var files = dir.listFiles();
         if (files != null) {
             for (var f : files) {
@@ -23,32 +34,33 @@ public class DependencyList {
             }
         }
     }
+
     private void GetFilesParents(File file) {
-        _resultMap.get(null).add(file.getAbsolutePath());
         try (BufferedReader fileReader = new BufferedReader(new FileReader(file))) {
+            boolean noParents = true;
             String line;
             do {
                 line = fileReader.readLine();
-                if (line.contains("require")) {
+                if (line != null && line.contains("require")) {
                     int index = line.indexOf('‘');
                     StringBuilder fileDir = new StringBuilder();
                     for (int i = line.indexOf('‘') + 1; line.charAt(i) != '’'; ++i) {
                         fileDir.append(line.charAt(i));
                     }
-                    fileDir.insert(0, _mainDir.getParent());
-                    if (_resultMap.containsKey(file.getAbsolutePath())) {
-                        _resultMap.get(file.getAbsolutePath()).add(fileDir.toString());
-                    } else {
+                    fileDir.insert(0, _mainDir.getParent() + "\\");
+                    if (!_resultMap.containsKey(file.getAbsolutePath())) {
                         _resultMap.put(file.getAbsolutePath(), new ArrayList<String>());
                     }
-                    /*String
-                    for ()*/
+                    _resultMap.get(file.getAbsolutePath()).add(fileDir.toString());
+                    noParents = false;
                 }
             } while (line != null);
+            if (noParents) {
+                _resultMap.get(null).add(file.getAbsolutePath());
+            }
         } catch (IOException e) {
             return;
         }
-
     }
     private final File _mainDir;
     private Map<String, List<String>> _resultMap;
